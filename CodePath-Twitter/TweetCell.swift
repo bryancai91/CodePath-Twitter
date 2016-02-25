e602670de74c42bac232abca93330e42b348bda7
Bryan Cai <caibryan@wustl.edu>
2016-02-25 17:12:48 -0600

    Finished Assignment 4

---
+++ CodePath-Twitter/TweetCell.swift
@@ -0,0 +1,52 @@
+//
+//  TweetCell.swift
+//  CodePath-Twitter
+//
+//  Created by Bryan Cai on 2/25/16.
+//  Copyright © 2016 Bryan Cai. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+	
+	
+	@IBOutlet weak var tweetImageView: UIImageView!
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var bodyLabel: UILabel!
+	@IBOutlet weak var timestampLabel: UILabel!
+	
+	var tweet: Tweet! {
+		didSet {
+			nameLabel.text = tweet.user?.name
+			tweetImageView.setImageWithURL((tweet.user?.profileImageUrl)!)
+			bodyLabel.text = tweet.text
+			
+			let dateFormatter = NSDateFormatter()
+			dateFormatter.dateStyle = .ShortStyle
+			let timestampString = dateFormatter.stringFromDate(tweet.timestamp!)
+			timestampLabel.text = timestampString
+		}
+	}
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+		
+		tweetImageView.layer.cornerRadius = 3
+		tweetImageView.clipsToBounds = true
+		nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+	}
+	
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CodePath-Twitter/TweetsViewController.swift
+++ CodePath-Twitter/TweetsViewController.swift
@@ -8,33 +8,57 @@
 
 import UIKit
 
-class TweetsViewController: UIViewController {
+class TweetsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
 	var tweets: [Tweet]!
+	@IBOutlet weak var tableView: UITableView!
 	
-    override func viewDidLoad() {
+	override func viewDidLoad() {
         super.viewDidLoad()
+		
+		tableView.delegate = self
+		tableView.dataSource = self
+		tableView.rowHeight = UITableViewAutomaticDimension
+		tableView.estimatedRowHeight = 120
+		
 		
 		TwitterClient.sharedInstance.homeTimeline({ (tweets: [Tweet]) -> () in
 			self.tweets = tweets
-			for tweet in tweets {
-				print(tweet.text)
-			}
+			self.tableView.reloadData()
+
+//			for tweet in tweets {
+//				print(tweet.text)
+//			}
 			}, failure: { (error: NSError) -> () in
 				print(error.localizedDescription)
 			})
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    } 
-    
+    }
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if tweets != nil {
+			return 20
+		}
+		else {
+			return 0
+		}
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("TweetCell", forIndexPath: indexPath) as! TweetCell
+		cell.tweet = tweets[indexPath.row]
+		return cell
+	}
+	
 	@IBAction func onLogout(sender: AnyObject) {
 		TwitterClient.sharedInstance.logout()
 	}
 
+	
     /*
     // MARK: - Navigation
 
